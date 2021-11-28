709b3b378f8aada0661547e0666c4de9a405d00f
Krzysztof Chojnacki <krzysztof.michal.chojnacki@gmail.com>
2021-11-28 08:51:42 +0100

    Update salesforce_selenium_na_brudno_dev.py

--- salesforce_selenium_na_brudno_dev.py
+++ salesforce_selenium_na_brudno_dev.py
@@ -19,6 +19,7 @@ from selenium.webdriver.support.ui import Select
 from datetime import datetime
 import time
 from simple_salesforce import Salesforce
+from selenium.common.exceptions import NoSuchElementException
 
 username = 'adammiklaski@gmail.com'
 password = 'Kjk78g9a'
@@ -38,12 +39,20 @@ iteracja = 1
 
 for powtorzenia in range (1, 3):
     time.sleep(5)
-    driver.find_element_by_xpath("//a[@title='New']").click()
+    try:
+        driver.find_element_by_xpath("//a[@title='New']").click()
+    except NoSuchElementException:
+        time.sleep(3)
+        driver.find_element_by_xpath("//a[@title='New']").click()
     now = datetime.now()
     dt_string = now.strftime("%d/%m/%Y_%H:%M:%S")
     
     time.sleep(1)
-    driver.find_element_by_xpath("//*[@Name='Name']").send_keys(f'Szansa_testowa_{dt_string}')
+    try:
+        driver.find_element_by_xpath("//*[@Name='Name']").send_keys(f'Szansa_testowa_{dt_string}')
+    except NoSuchElementException:
+        time.sleep(3)
+        driver.find_element_by_xpath("//*[@Name='Name']").send_keys(f'Szansa_testowa_{dt_string}')        
     driver.find_element_by_xpath("//*[@Name='CloseDate']").send_keys('10/11/2021')
     driver.find_element_by_xpath("/html/body/div[4]/div[2]/div/div[2]/div/div[2]/div/div/div/records-modal-lwc-detail-panel-wrapper/records-record-layout-event-broker/slot/records-lwc-detail-panel/records-base-record-form/div/div/div/div/records-lwc-record-layout/forcegenerated-detailpanel_opportunity___012000000000000aaa___full___create___recordlayout2/force-record-layout-block/slot/force-record-layout-section[1]/div/div/div/slot/force-record-layout-row[4]/slot/force-record-layout-item[2]/div/span/slot/slot/sfa-input-stage-name/force-record-picklist/force-form-picklist/lightning-picklist/lightning-combobox/div[1]/lightning-base-combobox/div/div[1]/input").click()
     driver.find_element_by_xpath("/html/body/div[4]/div[2]/div/div[2]/div/div[2]/div/div/div/records-modal-lwc-detail-panel-wrapper/records-record-layout-event-broker/slot/records-lwc-detail-panel/records-base-record-form/div/div/div/div/records-lwc-record-layout/forcegenerated-detailpanel_opportunity___012000000000000aaa___full___create___recordlayout2/force-record-layout-block/slot/force-record-layout-section[1]/div/div/div/slot/force-record-layout-row[4]/slot/force-record-layout-item[2]/div/span/slot/slot/sfa-input-stage-name/force-record-picklist/force-form-picklist/lightning-picklist/lightning-combobox/div[1]/lightning-base-combobox/div/div[1]/input").send_keys(Keys.ARROW_DOWN)
@@ -55,20 +64,31 @@ for powtorzenia in range (1, 3):
     #prospecting, qualification, needs analysis, value proposition
     #ID. decision making, perception analysis, proposal price quote
     #negotiation review, closed
-        mark_as_complete = driver.find_element_by_xpath("/html/body/div[4]/div[1]/section/div[1]/div[2]/div[2]/div[1]/div/div/div/div/div/one-record-home-flexipage2/forcegenerated-adg-rollup_component___force-generated__flexipage_-record-page___sfa__-opportunity_rec_-l___-opportunity___-v-i-e-w/forcegenerated-flexipage_opportunity_rec_l_opportunity__view_js/record_flexipage-record-page-decorator/div[1]/records-record-layout-event-broker/slot/slot/flexipage-record-home-with-subheader-template-desktop2/div/div[2]/slot/slot/flexipage-component2/slot/flexipage-aura-wrapper/div/div/article/div/div/div/div[1]/div[2]/button/span")
+        try:    
+            mark_as_complete = driver.find_element_by_xpath("/html/body/div[4]/div[1]/section/div[1]/div[2]/div[2]/div[1]/div/div/div/div/div/one-record-home-flexipage2/forcegenerated-adg-rollup_component___force-generated__flexipage_-record-page___sfa__-opportunity_rec_-l___-opportunity___-v-i-e-w/forcegenerated-flexipage_opportunity_rec_l_opportunity__view_js/record_flexipage-record-page-decorator/div[1]/records-record-layout-event-broker/slot/slot/flexipage-record-home-with-subheader-template-desktop2/div/div[2]/slot/slot/flexipage-component2/slot/flexipage-aura-wrapper/div/div/article/div/div/div/div[1]/div[2]/button/span")
+        except NoSuchElementException:
+            mark_as_complete = driver.find_element_by_xpath("/html/body/div[4]/div[1]/section/div[1]/div[2]/div[2]/div[1]/div/div/div/div/div/one-record-home-flexipage2/forcegenerated-adg-rollup_component___force-generated__flexipage_-record-page___sfa__-opportunity_rec_-l___-opportunity___-v-i-e-w/forcegenerated-flexipage_opportunity_rec_l_opportunity__view_js/record_flexipage-record-page-decorator/div[1]/records-record-layout-event-broker/slot/slot/flexipage-record-home-with-subheader-template-desktop2/div/div[2]/slot/slot/flexipage-component2/slot/flexipage-aura-wrapper/div/div/article/div/div/div/div[1]/div[2]/button/span")            
         driver.execute_script("arguments[0].click();", mark_as_complete)
         time.sleep(1)
     
-    time.sleep(2)
+    time.sleep(1)
     if iteracja == 1:
         #szansa wygrana
-        driver.find_element_by_xpath("/html/body/div[4]/div[2]/div/div[2]/div/div[2]/div/div/div/select").send_keys(Keys.ARROW_DOWN)
+        try:
+            driver.find_element_by_xpath("/html/body/div[4]/div[2]/div/div[2]/div/div[2]/div/div/div/select").send_keys(Keys.ARROW_DOWN)
+        except NoSuchElementException:
+            time.sleep(2)
+            driver.find_element_by_xpath("/html/body/div[4]/div[2]/div/div[2]/div/div[2]/div/div/div/select").send_keys(Keys.ARROW_DOWN)
         driver.find_element_by_xpath("/html/body/div[4]/div[2]/div/div[2]/div/div[2]/div/div/div/select").send_keys(Keys.ENTER)
         driver.find_element_by_xpath("/html/body/div[4]/div[2]/div/div[2]/div/div[3]/div/button[2]/span").click()
         
         #szansa przegrana
     if iteracja == 2:
-        driver.find_element_by_xpath("/html/body/div[4]/div[2]/div/div[2]/div/div[2]/div/div/div/select").send_keys(Keys.ARROW_DOWN)
+        try:
+            driver.find_element_by_xpath("/html/body/div[4]/div[2]/div/div[2]/div/div[2]/div/div/div/select").send_keys(Keys.ARROW_DOWN)
+        except NoSuchElementException:
+            time.sleep(2)
+            driver.find_element_by_xpath("/html/body/div[4]/div[2]/div/div[2]/div/div[2]/div/div/div/select").send_keys(Keys.ARROW_DOWN)            
         driver.find_element_by_xpath("/html/body/div[4]/div[2]/div/div[2]/div/div[2]/div/div/div/select").send_keys(Keys.ARROW_DOWN)
         driver.find_element_by_xpath("/html/body/div[4]/div[2]/div/div[2]/div/div[2]/div/div/div/select").send_keys(Keys.ENTER)
         driver.find_element_by_xpath("/html/body/div[4]/div[2]/div/div[2]/div/div[3]/div/button[2]/span").click()
@@ -90,12 +110,24 @@ opp_url = 'https://noname96-dev-ed.lightning.force.com/lightning/r/Opportunity/'
 for wystapienia in range (0,len(opps['records'])):
     driver.get(opp_url + opps['records'][wystapienia]['Id'] + '/view')
     time.sleep(3)
-    rozwin = driver.find_element_by_xpath("//*[@id='brandBand_2']/div/div/div/div/one-record-home-flexipage2/forcegenerated-adg-rollup_component___force-generated__flexipage_-record-page___sfa__-opportunity_rec_-l___-opportunity___-v-i-e-w/forcegenerated-flexipage_opportunity_rec_l_opportunity__view_js/record_flexipage-record-page-decorator/div[1]/records-record-layout-event-broker/slot/slot/flexipage-record-home-with-subheader-template-desktop2/div/div[1]/slot/slot/flexipage-component2/slot/records-lwc-highlights-panel/records-lwc-record-layout/forcegenerated-highlightspanel_opportunity___012000000000000aaa___compact___view___recordlayout2/force-highlights2/div[1]/div[1]/div[3]/div/runtime_platform_actions-actions-ribbon/ul/li[4]/lightning-button-menu/button/lightning-primitive-icon")
-    driver.execute_script("arguments[0].click();", rozwin)   
-    edytuj = driver.find_element_by_xpath("//*[@id='brandBand_2']/div/div/div/div/one-record-home-flexipage2/forcegenerated-adg-rollup_component___force-generated__flexipage_-record-page___sfa__-opportunity_rec_-l___-opportunity___-v-i-e-w/forcegenerated-flexipage_opportunity_rec_l_opportunity__view_js/record_flexipage-record-page-decorator/div[1]/records-record-layout-event-broker/slot/slot/flexipage-record-home-with-subheader-template-desktop2/div/div[1]/slot/slot/flexipage-component2/slot/records-lwc-highlights-panel/records-lwc-record-layout/forcegenerated-highlightspanel_opportunity___012000000000000aaa___compact___view___recordlayout2/force-highlights2/div[1]/div[1]/div[3]/div/runtime_platform_actions-actions-ribbon/ul/li[4]/lightning-button-menu/div/div/slot/runtime_platform_actions-action-renderer[3]/runtime_platform_actions-executor-page-reference/slot/slot/runtime_platform_actions-ribbon-menu-item/a/span")
+    try:
+        rozwin = driver.find_element_by_xpath("//*[@id='brandBand_2']/div/div/div/div/one-record-home-flexipage2/forcegenerated-adg-rollup_component___force-generated__flexipage_-record-page___sfa__-opportunity_rec_-l___-opportunity___-v-i-e-w/forcegenerated-flexipage_opportunity_rec_l_opportunity__view_js/record_flexipage-record-page-decorator/div[1]/records-record-layout-event-broker/slot/slot/flexipage-record-home-with-subheader-template-desktop2/div/div[1]/slot/slot/flexipage-component2/slot/records-lwc-highlights-panel/records-lwc-record-layout/forcegenerated-highlightspanel_opportunity___012000000000000aaa___compact___view___recordlayout2/force-highlights2/div[1]/div[1]/div[3]/div/runtime_platform_actions-actions-ribbon/ul/li[4]/lightning-button-menu/button/lightning-primitive-icon")
+    except NoSuchElementException:
+        time.sleep(3)
+        rozwin = driver.find_element_by_xpath("//*[@id='brandBand_2']/div/div/div/div/one-record-home-flexipage2/forcegenerated-adg-rollup_component___force-generated__flexipage_-record-page___sfa__-opportunity_rec_-l___-opportunity___-v-i-e-w/forcegenerated-flexipage_opportunity_rec_l_opportunity__view_js/record_flexipage-record-page-decorator/div[1]/records-record-layout-event-broker/slot/slot/flexipage-record-home-with-subheader-template-desktop2/div/div[1]/slot/slot/flexipage-component2/slot/records-lwc-highlights-panel/records-lwc-record-layout/forcegenerated-highlightspanel_opportunity___012000000000000aaa___compact___view___recordlayout2/force-highlights2/div[1]/div[1]/div[3]/div/runtime_platform_actions-actions-ribbon/ul/li[4]/lightning-button-menu/button/lightning-primitive-icon")        
+    driver.execute_script("arguments[0].click();", rozwin) 
+    try:
+        edytuj = driver.find_element_by_xpath("//*[@id='brandBand_2']/div/div/div/div/one-record-home-flexipage2/forcegenerated-adg-rollup_component___force-generated__flexipage_-record-page___sfa__-opportunity_rec_-l___-opportunity___-v-i-e-w/forcegenerated-flexipage_opportunity_rec_l_opportunity__view_js/record_flexipage-record-page-decorator/div[1]/records-record-layout-event-broker/slot/slot/flexipage-record-home-with-subheader-template-desktop2/div/div[1]/slot/slot/flexipage-component2/slot/records-lwc-highlights-panel/records-lwc-record-layout/forcegenerated-highlightspanel_opportunity___012000000000000aaa___compact___view___recordlayout2/force-highlights2/div[1]/div[1]/div[3]/div/runtime_platform_actions-actions-ribbon/ul/li[4]/lightning-button-menu/div/div/slot/runtime_platform_actions-action-renderer[3]/runtime_platform_actions-executor-page-reference/slot/slot/runtime_platform_actions-ribbon-menu-item/a/span")
+    except NoSuchElementException:
+        time.sleep(3)
+        edytuj = driver.find_element_by_xpath("//*[@id='brandBand_2']/div/div/div/div/one-record-home-flexipage2/forcegenerated-adg-rollup_component___force-generated__flexipage_-record-page___sfa__-opportunity_rec_-l___-opportunity___-v-i-e-w/forcegenerated-flexipage_opportunity_rec_l_opportunity__view_js/record_flexipage-record-page-decorator/div[1]/records-record-layout-event-broker/slot/slot/flexipage-record-home-with-subheader-template-desktop2/div/div[1]/slot/slot/flexipage-component2/slot/records-lwc-highlights-panel/records-lwc-record-layout/forcegenerated-highlightspanel_opportunity___012000000000000aaa___compact___view___recordlayout2/force-highlights2/div[1]/div[1]/div[3]/div/runtime_platform_actions-actions-ribbon/ul/li[4]/lightning-button-menu/div/div/slot/runtime_platform_actions-action-renderer[3]/runtime_platform_actions-executor-page-reference/slot/slot/runtime_platform_actions-ribbon-menu-item/a/span")        
     driver.execute_script("arguments[0].click();", edytuj)
     time.sleep(1)
-    driver.find_element_by_xpath("//*[@Name='Amount']").send_keys(0)
+    try:
+        driver.find_element_by_xpath("//*[@Name='Amount']").send_keys(0)
+    except NoSuchElementException:
+        time.sleep(3)
+        driver.find_element_by_xpath("//*[@Name='Amount']").send_keys(0)        
     time.sleep(1)
     driver.find_element_by_xpath("//*[@Name='SaveEdit']").click()
     time.sleep(2)
